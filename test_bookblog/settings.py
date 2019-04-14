41108ef1ef23e5eaf768fd1d425b325771f68595
Dide Mertens <d.g.j.mertens@gmail.com>
2019-04-14 13:22:52 +0100

    whitenoise

--- test_bookblog/settings.py
+++ test_bookblog/settings.py
@@ -31,12 +31,12 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
     'book_app',
 ]
