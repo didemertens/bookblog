925771e522da6b3fc857fa98a1847da0f4f7b90e
Dide Mertens <d.g.j.mertens@gmail.com>
2019-04-14 12:56:06 +0100

    adding static

--- test_bookblog/settings.py
+++ test_bookblog/settings.py
@@ -119,9 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATICFILES_DIRS = (
-os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 LOGIN_REDIRECT_URL = '/'
