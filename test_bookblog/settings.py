9d540985b28c100ca5c64321765f00687a589e38
Dide Mertens <d.g.j.mertens@gmail.com>
2019-04-22 19:08:58 +0100

    removed secret key

--- test_bookblog/settings.py
+++ test_bookblog/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+from secret_settings import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ajht5^y8w_-+l9&g1b9*nt4+p_gwt!+5zempz)2b-!&$k=c9gn'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
